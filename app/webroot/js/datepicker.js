45e26c59c370434dfe333382e918c3dbb3ca200f
Vitalie Danu <vitalied@assembla.com>
2012-10-23 19:09:15 +0300

    Convert tabs to spaces

--- app/webroot/js/datepicker.js
+++ app/webroot/js/datepicker.js
@@ -1,17 +1,17 @@
 jQuery.datepicker.setDefaults(
-	jQuery.extend(
-		{},
-		jQuery.datepicker.regional['ru'],
-		{
-			duration: '',
-			changeFirstDay: false,
-			showStatus: true,
-			showOn: 'button',
-			buttonImage: '/img/calendar.gif',
-			buttonImageOnly: true,
-			changeMonth: true,
-			changeYear: true,
-			showButtonPanel: true
-		}
-	)
+  jQuery.extend(
+    {},
+    jQuery.datepicker.regional['ru'],
+    {
+      duration: '',
+      changeFirstDay: false,
+      showStatus: true,
+      showOn: 'button',
+      buttonImage: '/img/calendar.gif',
+      buttonImageOnly: true,
+      changeMonth: true,
+      changeYear: true,
+      showButtonPanel: true
+    }
+  )
 );
